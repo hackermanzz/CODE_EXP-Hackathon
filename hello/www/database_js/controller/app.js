b4aefc15ace0695e405a50bda2e326f378a34cf4
Xerozz <60227509+Xerozzz@users.noreply.github.com>
2020-06-03 12:12:29 +0800

    APi

--- hello/www/database_js/controller/app.js
+++ hello/www/database_js/controller/app.js
@@ -12,3 +12,28 @@ app.use(bodyParser.json()) //Parse application/json
 app.use(urlendcodedParser) //Parse application/x-www-form-urlencoded
 
 //API calls
+//To return user information
+app.get('/api/user/:username',function (req,res){
+    var username = req.params.username
+    api.getUser(username, function(err,result){
+        if (!err){
+            res.send(result)
+        }
+        else{
+            res.status(500).send("Some Error")
+        }
+    })
+})
+
+//To return counsellor information
+app.get('/api/counsellor/:username',function (req,res){
+    var username = req.params.username
+    api.getUser(username, function(err,result){
+        if (!err){
+            res.send(result)
+        }
+        else{
+            res.status(500).send("Some Error")
+        }
+    })
+})
--- hello/www/database_js/model/api.js
+++ hello/www/database_js/model/api.js
@@ -1,6 +1,63 @@
 var db = require('./databaseConfig.js');
 var database = {
     //Insert functions here
+
+    //Check if user login info is correct
+    getUser: function (username, callback) {
+        var conn = db.getConnection()
+        conn.connect(function (err) {
+            //Error from connection detected
+            if (err) {
+                console.log(err);
+                return callback(err,null);
+            }
+            //Connection successful
+            else {
+                console.log("Connected!");
+                var sql = 'SELECT * FROM users WHERE username = ?';
+                //Array of values to replace the ? placeholder
+                conn.query(sql, [username], function (err, result) {
+                    conn.end();
+                    //If error is detected, we return a callback function with an error and null results
+                    if (err) {
+                        console.log(err);
+                        return callback(err,null);
+                    //If no error, result retrieved successfully and return a callback with null error and a result
+                    } else {
+                        return callback(null, result);
+                    }
+                });
+            }
+        });
+    },
+     //Check if counsellor login info is correct
+     getCounsellor: function (username, callback) {
+        var conn = db.getConnection()
+        conn.connect(function (err) {
+            //Error from connection detected
+            if (err) {
+                console.log(err);
+                return callback(err,null);
+            }
+            //Connection successful
+            else {
+                console.log("Connected!");
+                var sql = 'SELECT * FROM counsellors WHERE username = ?';
+                //Array of values to replace the ? placeholder
+                conn.query(sql, [username], function (err, result) {
+                    conn.end();
+                    //If error is detected, we return a callback function with an error and null results
+                    if (err) {
+                        console.log(err);
+                        return callback(err,null);
+                    //If no error, result retrieved successfully and return a callback with null error and a result
+                    } else {
+                        return callback(null, result);
+                    }
+                });
+            }
+        });
+    },
 }
 
 module.exports = database
